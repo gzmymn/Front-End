59cec46e9d49f24262f7c749ee8ad3910460bb93
Gizem Kübra Yaman <gizem.2406@gmail.com>
2021-03-21 00:25:50 +0300

    20 Mart Ders Sonu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
   {path:"", pathMatch:"full", component:ProductComponent}, //pathMatch:"full" bu bizim anasayfamıza eşit olacak başka şeyler gelirse hesaba katma demek
   {path:"products", component:ProductComponent},
-  {path:"products/category/:categoryId", component:ProductComponent}
+  {path:"products/category/:categoryId", component:ProductComponent},
+  {path:"products/add", component:ProductAddComponent}
 
 ]; //buraya yazdığım route'lar router-outlet'te ne göstereyim'in karşılığı
 
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel.';
 
 
 @Injectable({
@@ -22,5 +23,9 @@ export class ProductService {
   getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> { 
     let newPath =  this.apiUrl + "products/getbycategory?categoryId="+categoryId
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
+  add(product:Product):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add", product) //postman'deki body gibi düşün 
   }
 }
