d6e3e5dacfe591610a65784f76c9890352cabf13
Gizem Kübra Yaman <gizem.2406@gmail.com>
2021-03-25 02:08:01 +0300

    21.gün sonu(son ders)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductComponent } from './components/product/product.component';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   {path:"", pathMatch:"full", component:ProductComponent}, //pathMatch:"full" bu bizim anasayfamıza eşit olacak başka şeyler gelirse hesaba katma demek
   {path:"products", component:ProductComponent},
   {path:"products/category/:categoryId", component:ProductComponent},
-  {path:"products/add", component:ProductAddComponent}
+  {path:"products/add", component:ProductAddComponent, canActivate:[LoginGuard]},
+  {path:"login", component:LoginComponent}
 
 ]; //buraya yazdığım route'lar router-outlet'te ne göstereyim'in karşılığı
 
